0b5630b75a42b8eac9f4cb86af6ccda3befbd518
jakkyl <pantomime.jakkyl@gmail.com>
2014-08-09 13:35:44 -0400

    clean up made server call more generic

---
+++ SiteLinkMobileRestarted/app/src/main/java/citruscups/com/sitelinkmobile/DataStructures/DataTable.java
@@ -0,0 +1,65 @@
+package citruscups.com.sitelinkmobile.dataStructures;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by Jakkyl on 8/8/2014.
+ */
+public class DataTable
+{
+    private ArrayList<Map<String, Object>> _rows;
+    private String _name;
+    private boolean _closed;
+
+    public DataTable()
+    {
+        _rows = new ArrayList<Map<String, Object>>();
+        _name = "Table";
+    }
+
+    public boolean getClosed()
+    {
+        return _closed;
+    }
+
+    public void setClosed(boolean val)
+    {
+        _closed = val;
+    }
+
+
+    public String getName()
+    {
+        return _name;
+    }
+
+    public void setName(String val)
+    {
+        _name = val;
+    }
+
+    public ArrayList<Map<String, Object>> getRows()
+    {
+        return _rows;
+    }
+
+    public void addRow(Map<String, Object> row)
+    {
+        _rows.add(row);
+    }
+
+    @Override
+    public String toString()
+    {
+        String result = _name + ": ";
+
+        for (Map<String, Object> map : _rows)
+        {
+            result += "Row: { ";
+            result += map.toString() + "/n";
+        }
+
+        return result + " }";
+    }
+}
