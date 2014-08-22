8c7e0c21e7f996ee126e4cef3bf63cd995e04508
jakkyl <pantomime.jakkyl@gmail.com>
2014-08-22 00:13:15 -0400

    created. eventually get rid of different UsedFors

---
+++ SiteLinkMobileRestarted/app/src/main/java/citruscups/com/sitelinkmobile/helper/Constants.java
@@ -0,0 +1,15 @@
+package citruscups.com.sitelinkmobile.helper;
+
+/**
+ * Created by Jakkyl on 8/21/2014.
+ */
+public class Constants
+{
+    public enum UsedFor
+    {
+        Payment,
+        TenantLookup,
+        InqRes,
+        MoveIn
+    }
+}
