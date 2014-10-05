71171dd2cf3bb20310791600c8e3c0b4b8c054c3
jakkyl <pantomime.jakkyl@gmail.com>
2014-10-05 18:56:58 -0400

    added data param to executeText

--- SiteLinkMobileRestarted/app/src/main/java/citruscups/com/sitelinkmobile/interfaces/ICommand.java
+++ SiteLinkMobileRestarted/app/src/main/java/citruscups/com/sitelinkmobile/interfaces/ICommand.java
@@ -2,10 +2,11 @@ package citruscups.com.sitelinkmobile.interfaces;
 
 /**
  * Created by Jakkyl on 8/18/2014.
+ * Purpose:
  */
 public interface ICommand
 {
     public int executeColor(String text);
 
-    public String executeText(String text);
+    public String executeText(String text, Object data);
 }
